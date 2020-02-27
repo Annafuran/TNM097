43dcb69bc3e6298701b7e8c1c831a13b9544953e
Emma Algotsson <emmaalgotsson@c-10-253-197-90.clients.eduroam.liu.se>
2020-02-27 12:33:20 +0100

    Lagt till border

--- Projekt/createLegoBox.m
+++ Projekt/createLegoBox.m
@@ -14,15 +14,17 @@ yTop    = (size/4+0.5);
 yBottom = (size/4*3+0.5);
  
 
-%Diametern på pluppen hos en legobit är 4.8. 
-%Vi tar x4 för annars ser jag ingenting. 
-radius = (4.8*4)/2;
+%Diametern pï¿½ pluppen hos en legobit ï¿½r 4.8. 
+%Vi tar x4 fï¿½r annars ser jag ingenting. 
+radius = (4.8*2)/2;
 theta = linspace(0, 2*pi, round(4 * pi * radius));
 x1 = radius*cos(theta)+xLeft;
 y1 = radius*sin(theta)+yTop;
 x2 = radius*cos(theta)+xRight;
 y2 = radius*sin(theta)+yBottom;
 
+%length of one side 16x2 (32)
+lenSide = (16*2);
 
 for i = 1:length(colors)                   
     %Temp legopiece that will be put in the array of cells. 
@@ -34,6 +36,15 @@ for i = 1:length(colors)
     tempLegopiece(:,:,2) = rgbLegoColor(2);
     tempLegopiece(:,:,3) = rgbLegoColor(3);
     
+    %create a grey border
+    for k=1:length(lenSide)
+        rowSide = lenSide(k);
+        tempLegopiece(k,:,:) = 0.4;
+        tempLegopiece(:,k,:) = 0.4;
+        tempLegopiece(rowSide,:,:) = 0.4;
+        tempLegopiece(:,rowSide,:) = 0.4;
+    end
+         
     %Where to put our plupps. 
     for k = 1:length(x1)   
         rowLeft = round(y1(k));
@@ -43,8 +54,10 @@ for i = 1:length(colors)
         tempLegopiece(rowLeft, colTop, :) = 0;
         tempLegopiece(rowLeft, colBottom, :) = 0;
         tempLegopiece(rowRight, colTop, :) = 0;
-        tempLegopiece(rowRight, colBottom, :) = 0;      
+        tempLegopiece(rowRight, colBottom, :) = 0;   
+     
     end
+    
     %Add the legopiece to our box. 
     legopieces{i} = {tempLegopiece};    
 end
--- Projekt/mainfile.m
+++ Projekt/mainfile.m
@@ -6,21 +6,22 @@ legodata = readtable('colors.csv');
 colordata = legodata{:, 'rgb'}; %Contains only RGB data. 
 
 %Generates an array of cells.
-%En legobit är 15.8x15.8 --> 16x16
-%Vi förstorar x4 för annars såg jag ingenting. 
-legobox = createLegoBox(colordata, 64);
+%En legobit ï¿½r 15.8x15.8 --> 16x16
+%Vi fï¿½rstorar x4 fï¿½r annars sï¿½g jag ingenting. 
+%Ã¤ndra till 32 sa Sasan
+legobox = createLegoBox(colordata, 32);
 
 %Convert the cell to an array before displaying. (5 is the number on the
 %legopiece. 
 
 %Om man vill se en legobit. 
-testimage = cell2mat(legobox{5,1});
+testimage = cell2mat(legobox{30,1});
 imshow(testimage)
 imwrite(testimage, 'testis.png');
 
 %Om man vill se alla legobitar. Kaos. 
-%for i = 1:length(legobox)
-%testimage = cell2mat(legobox{i,1});
-%subplot(1,length(legobox),i)
-%imshow(testimage);
-%end
+% for i = 1:length(legobox)
+% testimage = cell2mat(legobox{i,1});
+% subplot(1,length(legobox),i)
+% imshow(testimage);
+% end
