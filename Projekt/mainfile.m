f8cad8a5c013f5f30627eabf7c3a214168f57aae
Annafuran <33253216+Annafuran@users.noreply.github.com>
2020-03-02 16:34:10 +0100

    You can now create a large image

--- Projekt/mainfile.m
+++ Projekt/mainfile.m
@@ -11,16 +11,21 @@ legobox = createLegoBox(colordata, 32);
 
 %Convert the cell to an array before displaying. (5 is the number on the
 %legopiece. 
+%org_image = imread('test_melon.p');
+
+%testimage = matchLego(org_image, legobox, 32);
 
 %Om man vill se en legobit. 
-testimage = cell2mat(legobox{30,1});
-imshow(testimage)
-grid on
-imwrite(testimage, 'testis.png');
+%testimage = cell2mat(legobox{30,1});
+%imshow(testimage)
+%grid on
+%imwrite(testimage, 'testis.png');
+
+testimage = {};
 
 %Om man vill se alla legobitar. Kaos. 
-% for i = 1:length(legobox)
-% testimage = cell2mat(legobox{i,1});
-% subplot(1,length(legobox),i)
-% imshow(testimage);
-% end
+ for i = 1:length(legobox)
+    temp_image = (cell2mat(legobox{i}));
+    testimage{i} = imtile(legobox{i});
+ end
+ montage(testimage)
