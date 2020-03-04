dabf93f813c2766f46dd91cbd42de45bee93ff3c
Fanny Andersson <fanzezi@gmail.com>
2020-03-04 12:57:50 +0100

    mainfile updated

--- Projekt/mainfile.m
+++ Projekt/mainfile.m
@@ -3,29 +3,60 @@ clc
 
 %Our lego color data. 
 legodata = readtable('colors.csv');
-colordata = legodata{:, 'rgb'}; %Contains only RGB data. 
+colordata = legodata{:, 'rgb'}; %Contains only HEX data (rgb is wrong label). 
 
 %Generates an array of cells.
-%(Storlek ska vara 32 enligt Sasan) 
-legobox = createLegoBox(colordata, 32);
-
+%En legobit �r 15.8x15.8 --> 16x16
+%Vi f�rstorar x4 f�r annars s�g jag ingenting.
+legosize = 32;
+legobox = createLegoBox(colordata, legosize);
+%createDatabase(legoBox);
 %Convert the cell to an array before displaying. (5 is the number on the
 %legopiece. 
-%org_image = imread('test_melon.p');
 
-%testimage = matchLego(org_image, legobox, 32);
 
 %Om man vill se en legobit. 
-%testimage = cell2mat(legobox{30,1});
+testimage = cell2mat(legobox{10,1});
 %imshow(testimage)
-%grid on
-%imwrite(testimage, 'testis.png');
-
-testimage = {};
+imwrite(testimage, 'testis.png');
 
 %Om man vill se alla legobitar. Kaos. 
- for i = 1:length(legobox)
+%for i = 1:length(legobox)
+%    testimage = cell2mat(legobox{i,1});
+%    subplot(1,length(legobox),i)
+%    imshow(testimage);
+%end
+
+% FIXA EGEN DATABAS
+%create folder with lego images
+ %for i = 1:length(legobox)
+ %    lego_list = cell2mat(legobox{i,1});
+ %    imwrite(lego_list, sprintf('Images/%d.png',i));
+ %end
+
+img = imread('watermelon.png');
+org_img_lab = rgb2lab(img);
+org_length = length(org_img_lab);
+
+myfolder = dir('/Users/FannyBanny/Documents/TNM097/TNM097/Projekt/images/*.png');
+numImg = length(myfolder);
+
+for i = 1:length(legobox)
     temp_image = (cell2mat(legobox{i}));
     testimage{i} = imtile(legobox{i});
- end
+end
  montage(testimage)
+
+for i = 1:numImg
+    %Load file
+    currFileName = myfolder(i).name;
+    % Color Correlation
+    currImage = imread(currFileName(:,:));
+    %test_img = matchLego(img, currImage, 32);
+    lego_img_lab = rgb2lab(currImage);
+end
+%Convert to LAB
+img_lab = rgb2lab(img);
+
+
+
